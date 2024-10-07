cc0a5c344ad2ee781cd1713b422af784773906f0
Abdalwanees <gabdo7682@gmail.com>
2024-10-07 20:59:53 +0300

    Add project files.

---
+++ UserMangement.PL/Models/ApplicationUsers.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace UserMangement.PL.Models
+{
+    public class ApplicationUsers:IdentityUser
+    {
+        [Required]
+        [MaxLength(100)]
+        public string FristName { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string LastName { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
---
+++ UserMangement.PL/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace UserMangement.PL.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
